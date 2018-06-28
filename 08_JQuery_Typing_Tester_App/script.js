c340d0c51e298eeaaf01cbd99cd2e288ed0afe74
Pankaj Kumar Singh <34129569+ps0305@users.noreply.github.com>
2018-06-29 01:37:13 +0530

    app development

--- 08_JQuery_Typing_Tester_App/script.js
+++ 08_JQuery_Typing_Tester_App/script.js
@@ -1,63 +1,83 @@
 
-
-var originalText = $("#original-text").text();
 var textArea = $('#text-area');
-
-
-var timer = 0;
 var minutes = 0;
 var seconds = 0;
 var milliSeconds = 0;
+var timer = 0;
+var timerRunning = false;
 
-
-    // Add leading zero to numbers 9 or below:
     function leadingZero(time) {
-        if(time <=9){
-            return "0"+time;
+        if(time<9){
+            return '0'+time;
         }else{
             return time;
         }
     }
-        // Start the timer:
+ function startTimer() {
+     minutes = Math.floor((timer/100)/60);
+     seconds = Math.floor((timer/100) - (minutes * 60));
+     milliSeconds = Math.floor(timer- (seconds * 100) - (minutes * 6000));
+    // Run a standard minute/second/hundredths timer:
+     timer++;
+     $('#minutes').text(leadingZero(minutes));
+     $('#seconds').text(leadingZero(seconds));
+     $('#milli-seconds').text(leadingZero(milliSeconds));
+
+ }
 
 
+ textArea.keypress(function () {
+     var textEnteredLen = $('#text-area').val().length;
+     if(textEnteredLen ===0 && !timerRunning){
+         interval = setInterval(startTimer,10);
+         timerRunning =true;
+     }
+ });
 
+    textArea.keyup(function () {
+        var textEntered = $('#text-area').val();
+        var originalText = $('#original-text').text();
+        var partialText = originalText.substr(0,textEntered.length);
+        var message =$('#message');
+        var messageCard = $('#message-card');
 
-//minutes = Math.floor((timer/100)/60);
-//seconds = Math.floor((timer/100) - (minutes * 60));
-//milliSeconds = Math.floor(timer- (seconds * 100) - (minutes * 6000));
-// Run a standard minute/second/hundredths timer:
-
-    function startTimer() {
-        minutes = Math.floor((timer/100)/60);
-        seconds = Math.floor((timer/100) - (minutes * 60));
-        milliSeconds = Math.floor(timer - (seconds * 100) - (minutes * 6000));
-
-        $('#minutes').text(leadingZero(minutes));
-        $('#seconds').text(leadingZero(seconds));
-        $('#milli-seconds').text(leadingZero(milli-seconds));
-        timer++;
-    }
-
-
-    // Reset everything:
-    $('#reset-button').click(function() {
-        $('#text-area').val('');
-        $('#minutes').text('00');
-        $('#seconds').text('00');
-        $('#milli-seconds').text('00');
-});
-
-    // Event listeners for keyboard input and the reset button:
-
-
-        textArea.keypress(function (){
-            var textEnteredLen = $('#text-area').val().length;
-            if(textEnteredLen === 0){
-                //start timer
-            
-            setInterval(startTimer,10);
+        if(textEntered.length ===0){
+            message.text('');
+            messageCard.removeClass('bg-success').removeClass('bg-teal')
+                .removeClass('bg-danger').addClass('bg-light');
+        }else{
+            if(textEntered === originalText){
+                message.text('Congratulations !!!');
+                messageCard.removeClass('bg-light').removeClass('bg-teal')
+                    .removeClass('bg-danger').addClass('bg-success');
+                clearInterval(interval);
+            }else{
+                if(textEntered === partialText){
+                    message.text('Correct !!');
+                    messageCard.removeClass('bg-light').removeClass('bg-success')
+                        .removeClass('bg-danger').addClass('bg-teal');
+                }else{
+                    message.text('Wrong !!');
+                    messageCard.removeClass('bg-success').removeClass('bg-teal')
+                        .removeClass('bg-light').addClass('bg-danger');
+                }
+            }
         }
-});
-       
+    });
 
+$('#reset-button').click(function() {
+    clearInterval(interval);
+    timer = 0;
+    minutes = 0;
+    seconds = 0;
+    milliSeconds = 0;
+    interval = 0;
+    timerRunning = false;
+    textArea.val('');
+    $('#message').text('');
+    $('#message-card').removeClass('bg-success').removeClass('bg-teal')
+        .removeClass('bg-danger').addClass('bg-light');
+    $('#minutes').text('00');
+    $('#seconds').text('00');
+    $('#milli-seconds').text('00');
+});
